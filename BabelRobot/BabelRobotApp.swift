d995230e0e302e966aeb520acfc311240986ddfc
MartinezAvellan <martinez.avellan@hotmail.com>
2026-06-02 17:56:18 +0200

    Initial Commit

---
+++ BabelRobot/BabelRobotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabelRobotApp.swift
+//  BabelRobot
+//
+//  Created by Hugo A Martinez on 02/06/26.
+//
+
+import SwiftUI
+
+@main
+struct BabelRobotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
